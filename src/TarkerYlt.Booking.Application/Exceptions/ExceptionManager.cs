785f4dca5c027bbd41fa78a2dd539d7058c85cff
Yeison Londoño Tabarez <ylondonotz@gmail.com>
2024-03-23 03:24:55 -0500

    Create ExceptionManager.cs

---
+++ src/TarkerYlt.Booking.Application/Exceptions/ExceptionManager.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using TarkerYlt.Booking.Application.Features;
+
+namespace TarkerYlt.Booking.Application.Exceptions
+{
+    public class ExceptionManager : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            context.Result = new ObjectResult(ResponseApiService.Response(StatusCodes.Status500InternalServerError, context.Exception.Message, null));
+            context.HttpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        }
+    }
+}
