82ad348389de974bc4de7ca556c7e6ad135a41cd
Yeison Londoño Tabarez <ylondonotz@gmail.com>
2024-03-23 02:50:58 -0500

    12_53: Configuración general de APIs
    Sección 12: Configuración general de APIs
    51. Crear modelo de respuesta

---
+++ src/TarkerYlt.Booking.Application/Features/ResponseApiService.cs
@@ -0,0 +1,25 @@
+﻿using TarkerYlt.Booking.Domain.Models;
+
+namespace TarkerYlt.Booking.Application.Features
+{
+    public static class  ResponseApiService //Static para no tener necesidad de instanciar metodos o la clase
+    {
+        public static BaseResponseModel Response(
+            int statusCode, string message = null, dynamic data = null)
+        {
+            bool success = false; 
+
+            if (statusCode >= 200 && statusCode < 300)
+                success = true;
+
+            var result = new BaseResponseModel
+            {
+                StatusCode = statusCode,
+                Success = success,
+                Message = message,
+                Data = data
+            };
+            return result;
+        }
+    }
+}
---
+++ src/TarkerYlt.Booking.Domain/Models/BaseResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TarkerYlt.Booking.Domain.Models
+{
+    public class BaseResponseModel
+    {
+        public int StatusCode { get; set; } //Codigo de estado
+        public bool Success { get; set; } //Si se ejecuto o no
+        public string Message { get; set; }
+        public dynamic Data { get; set; } //setear cualquier dato, puedo acceder a propiedades sin necesidad de instanciar
+    }
+}
